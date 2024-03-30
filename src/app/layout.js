59147faa93a17f3fc512e082ef79528bb3413c1b
Paul Joseph Pineda <pauljosephpineda@Pauls-iMac.local>
2024-03-30 14:47:09 -0300

    v3

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
+      <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
       <body className={inter.className}>{children}</body>
     </html>
   );
