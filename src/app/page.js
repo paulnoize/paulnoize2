325848700809044824e361a3b963e11e5b8a8482
Paul Joseph Pineda <pauljosephpineda@Pauls-iMac.local>
2024-03-30 15:02:23 -0300

    v6

--- src/app/page.js
+++ src/app/page.js
@@ -17,8 +17,8 @@ export default function Home() {
   return (
     <main className="flex items-center justify-center h-screen">
       <div className="w-full flex items-center justify-center">
-        <div className="orbit bg-transparent border-2 border-gray-500 rounded-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></div>
-        <div className="planet bg-red-500 rounded-full absolute w-12 h-12 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></div>
+      <div className="orbit"></div>
+        <div className="planet"></div>
         <Swiper
           className="w-full"
           modules={[EffectFade, Autoplay]} 
